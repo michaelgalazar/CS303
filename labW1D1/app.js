8ed43b0f9e14ece0dd0013b8f8ad8962b4d53de3
michaelgalazar <79475036+michaelgalazar@users.noreply.github.com>
2021-03-25 02:44:28 -0700

    update

--- labW1D1/app.js
+++ labW1D1/app.js
@@ -2,12 +2,21 @@
 /* eslint-disable no-unused-vars */
 "use strict";
 
+exports.pow = pow;
+exports.add2 = add2;
+exports.add = add;
+exports.computeGrade = computeGrade;
+
 function pow(x, n) {
-	return x * n;
+	let power = 1;
+	for ( let i = 0; i < n; i++ ) {
+		power *= x;
+	}
+	return power;
 }
 
-function add2(n1, n2) {
-	return n1 + n2;
+function add2(n1, n2, n3) {
+	return n1 + n2 + n3;
 }
 
 function add(...args) {
@@ -30,6 +39,29 @@ function add(...args) {
 	0 - 59   NC
 */
 function computeGrade(grades) {
-	return "???";
+
+	let sum = 0;
+	let average = 0;
+	for ( let i = 0; i < grades.length; i++) {
+		sum += grades[i];
+	}
+	average = sum / grades.length;
+
+	if ( average >=90 && average <= 100 ) {
+		return "A";
+	}
+	if ( average >=80 && average <= 89 ) {
+		return "B";
+	}
+	if ( average >=70 && average <= 79 ) {
+		return "C";
+	}
+	if ( average >=60 && average <= 69 ) {
+		return "D";
+	}
+	if ( average >=0 && average <= 59 ) {
+		return "NC";
+	}
+	
 }
 
