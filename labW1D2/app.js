6ec0356c1ac3f70cbb6667a84d96eb093df92d75
michaelgalazar <79475036+michaelgalazar@users.noreply.github.com>
2021-03-27 01:46:57 -0700

    Update app.js

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -29,6 +29,36 @@ function nameString(obj) {
             array.push(key);
         }
 
-        return array.sort().join(".");    //array[0] + "." + array[array.length - 1] + "." + array[1];
+        output = array.sort().join(".");
+        return output;    //array[0] + "." + array[array.length - 1] + "." + array[1];
     }
 }
+
+// function nameString(obj) {
+    
+//     if (obj.length === 0) {
+//         return "object has no properties";
+//     } 
+//     else if (obj === undefined) {
+//         return "no object passed";
+//     } 
+//     else if (obj === null) {
+//         return "null argument";
+//     } 
+//     else if (obj !== 0) {
+//         let newArray = [];
+//         for (let key in obj) {
+           
+//             if (obj[key] > 1) {
+//                 newArray.push(key);
+//             } else
+//                 key  //Don't need this
+//         }
+//         //console.log(newArray);
+//         newArray.sort();
+//         let newsortedArray=newArray.join(".");
+//         //console.log(newsortedArray);
+//         return newsortedArray;
+// }
+
+//     }
