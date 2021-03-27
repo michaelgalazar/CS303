6583c63d9d52687bca39939b038cb54875bfc555
michaelgalazar <79475036+michaelgalazar@users.noreply.github.com>
2021-03-27 01:52:39 -0700

    Update app.js

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -7,7 +7,7 @@ exports.nameString = nameString;
 // See tests.js for behavior
 function nameString(obj) {
 
-    if ( Object.keys(obj).length === 0 ) {
+    if ( obj.length === 0 ) {
         return "object has no properties";
     }
 
