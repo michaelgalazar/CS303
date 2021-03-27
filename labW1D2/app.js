ed0f4d6442c94d5a08e492ad02d72ae0833dddbb
michaelgalazar <79475036+michaelgalazar@users.noreply.github.com>
2021-03-26 20:05:05 -0700

    Update app.js

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -3,28 +3,54 @@
 "use strict";
 
 // See tests.js for behavior
+// function nameString(obj) {
+
+//     if ( Object.keys(obj).length === 0 ) {
+//         return "object has no properties";
+//     }
+
+//     else if ( obj === undefined ) {
+//         return "no object passed";
+//     }
+//     else if ( obj === null ) {
+//         return "null argument";
+//     }
+//     else if ( arguments.length == 1 ) {
+//         for (let key in obj) {
+//             return key;
+//         }
+//     }
+//     else {
+//         let array = [];
+//         for (let key in obj) {
+//             array.push(key);
+//         }
+//         return array[0] + "." + array[array.length - 1] + "." + array[1];
+//     }
+// }
+
 function nameString(obj) {
-
-    if ( Object.keys(obj).length === 0 ) {
+    
+    if (obj == 0) {
         return "object has no properties";
-    }
-
-    else if ( obj === undefined ) {
+    } else if (obj === undefined) {
         return "no object passed";
-    }
-    else if ( obj === null ) {
+    } else if (obj === null) {
         return "null argument";
-    }
-    else if ( arguments.length == 1 ) {
+    } else if (obj !== 0) {
+        let newArray = [];
         for (let key in obj) {
-            return key;
+           
+            if (obj[key] > 1) {
+                newArray.push(key);
+            } else
+                key  //Don't need this
         }
-    }
-    else {
-        let array = [];
-        for (let key in obj) {
-            array.push(key);
-        }
-        return array[0] + "." + array[array.length - 1] + "." + array[1];
-    }
+        //console.log(newArray);
+        newArray.sort();
+        let newsortedArray=newArray.join(".");
+        //console.log(newsortedArray);
+        return newsortedArray;
 }
+
+    }
