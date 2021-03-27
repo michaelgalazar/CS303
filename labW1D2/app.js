1e37aac12a4f39c69ff639c8eeb6b00ccd70024a
michaelgalazar <79475036+michaelgalazar@users.noreply.github.com>
2021-03-26 20:08:57 -0700

    Update app.js

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -2,55 +2,58 @@
 /* eslint-disable no-unused-vars */
 "use strict";
 
-// See tests.js for behavior
-// function nameString(obj) {
-
-//     if ( Object.keys(obj).length === 0 ) {
-//         return "object has no properties";
-//     }
-
-//     else if ( obj === undefined ) {
-//         return "no object passed";
-//     }
-//     else if ( obj === null ) {
-//         return "null argument";
-//     }
-//     else if ( arguments.length == 1 ) {
-//         for (let key in obj) {
-//             return key;
-//         }
-//     }
-//     else {
-//         let array = [];
-//         for (let key in obj) {
-//             array.push(key);
-//         }
-//         return array[0] + "." + array[array.length - 1] + "." + array[1];
-//     }
-// }
-
+See tests.js for behavior
 function nameString(obj) {
-    
-    if (obj == 0) {
+
+    if ( Object.keys(obj).length === 0 ) {
         return "object has no properties";
-    } else if (obj === undefined) {
+    }
+
+    else if ( obj === undefined ) {
         return "no object passed";
-    } else if (obj === null) {
+    }
+    else if ( obj === null ) {
         return "null argument";
-    } else if (obj !== 0) {
-        let newArray = [];
+    }
+    else if ( arguments.length == 1 ) {
         for (let key in obj) {
-           
-            if (obj[key] > 1) {
-                newArray.push(key);
-            } else
-                key  //Don't need this
+            return key;
         }
-        //console.log(newArray);
-        newArray.sort();
-        let newsortedArray=newArray.join(".");
-        //console.log(newsortedArray);
-        return newsortedArray;
+    }
+    else {
+        let array = [];
+        for (let key in obj) {
+            array.push(key);
+        }
+        return array[0] + "." + array[array.length - 1] + "." + array[1];
+    }
 }
 
-    }
+// function nameString(obj) {
+    
+//     if (obj.length === 0) {
+//         return "object has no properties";
+//     } 
+//     else if (obj === undefined) {
+//         return "no object passed";
+//     } 
+//     else if (obj === null) {
+//         return "null argument";
+//     } 
+//     else if (obj !== 0) {
+//         let newArray = [];
+//         for (let key in obj) {
+           
+//             if (obj[key] > 1) {
+//                 newArray.push(key);
+//             } else
+//                 key  //Don't need this
+//         }
+//         //console.log(newArray);
+//         newArray.sort();
+//         let newsortedArray=newArray.join(".");
+//         //console.log(newsortedArray);
+//         return newsortedArray;
+// }
+
+//     }
